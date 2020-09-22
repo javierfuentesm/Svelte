1669a4f1fd00f1d74fa851de2efe83798fcb67ae
Javier <javier.fuentesm@hotmail.com>
2020-09-22 15:50:55 -0400

    Ciclo de vida

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
   import Person from './components/Person.svelte'
   import Skills from './components/Skills.svelte'
   import Input from './components/Input.svelte'
+  import Photos from './components/Photos.svelte'
+
   export let name;
   export let lastName = "Fuentes";
 
@@ -56,5 +58,7 @@
   <Person {...data} />
   <Skills/>
   <Input />
+  <Photos />
+
 
 </main>
